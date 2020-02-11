e55b045d44fc5439335a70ae823a6fec2384433a
Pavel <pavlogenizoy@gmail.com>
2020-02-11 18:20:00 +0200

    First commit

---
+++ Lesson1OfCourses.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << R"(My first project	
+										ggg)" << '\n';
+	std::cout << 05 << std::endl;
+	std::cout << 0xF1 << std::endl;
+	std::cout << 1.25 << std::endl;
+	return 0;
+}
