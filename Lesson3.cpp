fee690fe30396ed444819381097a07cb0de92d21
Pavel <pavlogenizoy@gmail.com>
2020-02-24 00:53:04 +0200

    next lessons

---
+++ Lesson3.cpp
@@ -0,0 +1,144 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <bitset>
+
+int main()
+{
+	
+	int value{ 10 };
+	int value2{ 20 };
+	std::cout << std::boolalpha;
+	bool is_state = (value == value2);
+	std::cout << is_state << std::endl;
+	is_state = (value != value2);
+	std::cout << is_state << std::endl;
+	is_state = (value >= value2);
+	std::cout << is_state << std::endl;
+	std::cout << "//////////////////////////////////////" << std::endl;
+	int a = 41;
+	// проверка диапазона (20, 40] 
+	bool is_access = a > 20 && a <= 40;
+	std::cout << is_access << std::endl;
+
+	is_access = a == 20 || a == 41;
+	std::cout << is_access << std::endl;
+
+	is_access = !(a == 20 || a == 41);
+	std::cout << is_access << std::endl;
+	std::cout << "//////////////////////////////////////" << std::endl;
+	std::bitset<8> bit{ 126 }; // 8 - сколько бит всего
+	std::cout << bit.to_string('X') << std::endl;
+	bit.set(2, false); // 2 - это 3-тий бит который сбрасываем (3 ибо отсчёт от 0)
+	std::cout << bit.to_string('X') << std::endl;
+	std::cout << bit.count() << std::endl;
+	std::cout << "//////////////////////////////////////" << std::endl;
+	std::cout << (value & 1) << std::endl;
+	auto value1{ 123456 };
+	std::cout << value1 % 10 << std::endl; // при 100 то ласт 2 цифры
+	std::cout << "//////////////////////////////////////" << std::endl;
+
+	const long long kMax = std::numeric_limits<int>::max();
+	int k;
+	std::cin >> k;
+	/*if (a <= max)
+	{											// но так не надо делать  (точка 1 входа (так называется))
+		int b;
+		std::cin >> b;
+		if (b <= max)
+		{
+			int c;
+			std::cin >> c;
+			if (c <= max)
+			{
+				auto D = a * b*c;
+				std::cout << D << std::endl;
+			}
+			else {
+				std::cout << "Error c";
+			}
+		}
+		else {
+			std::cout << "Error b";
+		}
+	}
+	else {
+		std::cout << "Error a";
+	}
+	*/
+	if (k > kMax) {
+		std::cout << "Error k" << std::endl;
+		return 0;
+	}
+	int b;
+	std::cin >> b;
+	if (b > kMax) {
+		std::cout << "Error b" << std::endl;
+		return 0;
+	}
+	auto D = k * b;
+	std::cout << D << std::endl;
+	std::cout << "//////////////////////////////////////" << std::endl;
+	
+	enum {
+		FIRST=1, // по умолчанию =0 (даже не пишут чему равно)
+		SECOND,
+		UNDEFINED  // вместо default в свитче
+	};
+	/*
+	enum {
+		A = 'A',
+		B,
+		C
+	};
+	enum {
+		FIRST_BIT = 1LL << 0;
+		SECOND_BIT = 1LL << 1;       // 0001  -->  0010   , сдвиг влево 
+	};
+	*/
+	//case 1 .. 10     // так тоже можно
+	
+
+	int v_1;
+	std::cin >> v_1;
+
+	switch (v_1)
+	{
+	case FIRST:
+		std::cout << "1" << std::endl;
+		break;   // если break не будет то произойдёт провал, т.е. пойдёт дальше пока не найдёт некст break (вместо break можно юзать return)
+	case SECOND:
+		std::cout << "2" << std::endl;
+		break;
+	case UNDEFINED:
+		std::cout << "UNDEFINED" << std::endl;
+		break;
+	default:
+		std::cout << "not match" << std::endl;
+	}
+	std::cout << "//////////////////////////////////////" << std::endl;
+
+	int bro;
+	std::cin >> bro;
+	for (size_t i = 0; i < bro; i++)  // size_t максимальный размер на любой платформе (для предотвращения переполнения)
+	{
+		std::cout << "before check\n";
+	}
+	std::cout << "//////////////////////////////////////" << std::endl;
+
+	int rab{ 123456 };
+	for (auto rab_copy{ rab }; rab_copy >0 ; rab_copy/=10)
+	{
+		if (((rab_copy % 10) % 2) == 0)
+		{
+			std::cout << "even" << std::endl;
+		}
+		else {
+			std::cout << "odd" << std::endl;
+		}
+	}
+	std::cout << "//////////////////////////////////////" << std::endl;
+
+
+
+	return 0;
+}
---
+++ Lesson4.cpp
@@ -0,0 +1,84 @@
+﻿#include "pch.h"
+#include <iostream>
+
+enum {
+	FIRST,
+	SECOND,
+	LAST
+};
+
+int main()
+{	
+	int array[2];
+	int array3[2] {1,2};
+	int array1[10]{ 1 };  //only 1 element is 1, another - 0
+	// int mak[2]{};  all elements are 0
+	// int array[] {1,2,3};  компилятор сам подберёт размер
+	const int kArray[LAST]{54,55};
+
+	std::cout << array[1] << std::endl; //если не инициализированы элемменты массива то выведит мусор (т.е. значение, той ячейки к которой обращаемся)
+	std::cout << array3[1] << std::endl;
+	std::cout << array1[1] << std::endl;
+	std::cout << "//////////////////////////////" << std::endl;
+	/*int i;
+	if (i < LAST) {
+		kArray[i]
+	}*/
+
+	char kArr[]{ "HELLOWORLD" }; //для массива символов размер не указуем (всегда размер = к-ство символов + 1 (скрытый 0-терминальной символ))
+	// char kArr[]{'H','E'};   тут размер = к-ству символов
+
+	// размер массива всегда задаём отдельной константой с размером size_t
+	const size_t kSizeArray{ 3 };
+	int Ark[kSizeArray]{ 1,2,3 };
+	for (size_t i = 0; i < 3; i++)
+	{
+		std::cout << Ark[i] << std::endl;
+	}
+	std::cout << "//////////////////////////////" << std::endl;
+
+	int val{ 1 };
+	std::cout << sizeof(val) << std::endl;
+	std::cout << sizeof(Ark) << std::endl;  // размер будет 4 * 3 = 12  (количество выделяемой памяти)
+	std::cout << sizeof(Ark)/sizeof(Ark[0]) << std::endl; // сам размер массива, т.е. именно сколько элементов в нём (ответ = 3)
+	std::cout << "//////////////////////////////" << std::endl;
+
+	for (auto i : Ark) {
+		std::cout << i << std::endl;    // более упрощённая версия цыкла for
+	}
+
+	std::cout << kArr << std::endl; // символьные массив выводится изи
+	std::cout << "//////////////////////////////" << std::endl;
+
+	int vak{ 10 };
+	int *vak_ptr; //обявляем некую переменную которая ссылается на чтото
+	int *vaks_ptr = &vak; // указатель указывает на переменную интового типа (это типа помимо указателя на адресс)
+	std::cout << &vak << " " << &vak_ptr << std::endl;
+	std::cout << vak << " " << vak_ptr << std::endl;
+
+	std::cout << &vak << " " << &vaks_ptr << std::endl;
+	std::cout << vak << " " << vaks_ptr << std::endl;
+	std::cout << "//////////////////////////////" << std::endl;
+
+	*vaks_ptr = 100; // разименование, т.е. мы заменили значение той ячейки на адресс которой указывает указатель
+	std::cout << vak << " " << *vaks_ptr << std::endl;
+	/*
+	// проверка валидности указателя
+	int *var_ptr = nullptr;
+
+	if (var_ptr != nullptr) {
+		*var_ptr = 100;
+	}
+	*/
+	double d{ 1.23 };
+	//vaks_ptr = &d;    ошибка так как разные типы данных
+	void* ptr=&d; // нетипизированный указатель, то есть он  банально хранит адресс переменной  (типа воид стирает типы данных)
+
+	*(static_cast<double*>(ptr)) = 300; // разименование явной типизации
+	std::cout << d << std::endl;
+	/*
+	*(static_cast<int*>(ptr)) = 100;     // здесь идёт повреждение ячейки памяти (double) ибо пытаемся представить его в виде инта
+	std::cout << d << std::endl;
+	*/
+	return 0;
+}
